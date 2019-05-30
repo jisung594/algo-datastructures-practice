0c32fd8026ae44fdadc12e745021c460c477c1bd
jisung594 <jisung594@gmail.com>
2019-05-29 21:12:08 -0400

    linked list practice

--- practice.js
+++ practice.js
@@ -1,22 +1,62 @@
-function addTwoTo(n) {
-  console.log(n)
-// str becomes the output of the recursive call
-  if (n < 10){
-    addTwoTo(n + 2)
-  } else {
-    return true
-  }
-}
-
-addTwoTo(0)
-
-// ------------------------------------------------ //
-
 class Node {
   constructor(data, next = null, prev = null) {
     this.data = data
     this.next = next
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  prepend(node) {
+    let oldHead = this.head
+    this.head = node
+    this.head.next = oldHead
+
+// // ***** alternative way *****
+//     node.next = this.head;
+//     this.head = node;
+//     return this.head;
+  }
+
+  append(node) {
+    if (!this.head) {
+      this.head = node
+    } else {
+      let currentHead = this.head
+
+      while (currentHead.next !== null) {
+        currentHead = currentHead.next
+        console.log(currentHead.data)
+      }
+
+      currentHead.next = node
+    }
+  }
+}
+
+
+const node4 = new Node("d")
+const node3 = new Node("c", node4)
+const node2 = new Node("b", node3)
+
+let alphabetList = new LinkedList()
+alphabetList.head = node2
+
+
+const node5 = new Node("e")
+alphabetList.append(node5)
+
+const node1 = new Node("a", node2)
+alphabetList.prepend(node1)
+
+console.log(alphabetList.head.next)
+
+
+
+ 
+
 
  
